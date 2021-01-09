c072bc9c65c6f04b0656c7a9ec60eff15b4f7cc9
gayathri0803 <gayathri.nambur@gmail.com>
2021-01-10 00:53:40 +0530

    Heroku Deployment Files

---
+++ app.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTMI GUI
+    '''
+    exp = request.form.get('experience')
+    test_score = request.form.get('test_score')
+    inter_score = request.form.get('interview_score')
+
+
+    # Convert all the features into int
+    int_features = [ int(exp), int(test_score), int(inter_score)]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0],2)
+
+    return render_template('index.html', prediction_test='Employee salary should be $ {}'.format(output))
+if __name__ == '__main__':
+    app.run(debug=True)
